962fb3560ad36707c7126e3a38b9216523957036
Kamil Romanowicz <kamil.romanowicz95@gmail.com>
2024-08-13 11:00:08 +0200

    update

--- bakebuddy/src/components/Sidebar/Sidebar.tsx
+++ bakebuddy/src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,10 @@
 import { useNavigate } from "react-router-dom"
 import { useState } from "react";
+import SidebarIcon from '../../assets/sidebar-icon.png'
+import BreakfastIcon from '../../assets/breakfast-icon.png'
+import LunchIcon from '../../assets/lunch-icon.png'
+import DinnerIcon from '../../assets/dinner-icon.png'
+import BakingIcon from '../../assets/baking-icon.png'
 import './Sidebar.scss'
 
 function Sidebar() {
@@ -31,7 +36,7 @@ function Sidebar() {
     return (
         <div className={`sidebar ${isOpen ? 'open' : ''}`} >
             <button className="toggle_btn" onClick={handleSideBarChange}>
-                Toggle Sidebar
+                <img src={SidebarIcon} alt="" className="sidebar-icon" />
             </button>
             <section className="sidebar-head">
                 <h2>Recipes</h2>
@@ -40,24 +45,28 @@ function Sidebar() {
                 <li className="nav-item-1">
                     <a href="" className="nav-link" onClick={goToBaking}>
                         <i></i>
+                        <img src={BakingIcon} alt="" className="sidebar-icons" />
                         <span>Baking</span>
                     </a>
                 </li>
                 <li className="nav-item-2">
                     <a href="" className="nav-link" onClick={goToBreakfast}>
                             <i></i>
+                            <img src={BreakfastIcon} alt="" className="sidebar-icons" />
                             <span>Breakfast</span>
                         </a>
                 </li>
                 <li className="nav-item-3">
                     <a href="" className="nav-link" onClick={goToLunch}>
                             <i></i>
+                            <img src={LunchIcon} alt="" className="sidebar-icons" />
                             <span>Lunch</span>
                         </a>
                 </li>
                 <li className="nav-item-4">
                     <a href="" className="nav-link" onClick={goToDinner}>
                             <i></i>
+                            <img src={DinnerIcon} alt="" className="sidebar-icons" />
                             <span>Dinner</span>
                         </a>
                 </li>
